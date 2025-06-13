f72d6f83bc13dcd2bd03184e9a692efdd7e498f6
sharipovikromjon <ikromjonsharipov924@gmail.com>
2025-06-13 15:06:11 +0500

    initialize React Todo List application with main components and configuration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div>
+      <TodoInput /> <TodoList />
+    </div>
+  );
+}
+
+export default App;
